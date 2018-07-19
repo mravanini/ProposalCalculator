311029f59aea9c415925ca73a21924a15b4b8d13
angelo <angelo.carvalho@gmail.com>
2018-07-19 20:14:01 -0300

    bug fix

--- src/main/java/com/amazon/proposalcalculator/calculator/RunOnServer.java
+++ src/main/java/com/amazon/proposalcalculator/calculator/RunOnServer.java
@@ -141,7 +141,7 @@ public class RunOnServer {
 
 				// calculate and delete message from SQS
 				//String outputFileName = getCurrentTime() + "_" + Constants.OUTPUT_FILE_NAME;
-				String outputFileName = "output_"+ getCurrentTime() + "_" + inputFileS3Key.split("/")[1];
+				String outputFileName = "output_"+ getCurrentTime() + "_" + inputFileName;
 				
 				Calculator.calculate(instanceInputs, outputFileName);
 				sqsClient.deleteMessage(new DeleteMessageRequest(myQueueUrl, messageReceiptHandle));
